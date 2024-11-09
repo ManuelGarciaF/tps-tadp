bad6e1c1ee4ee3a5b551784096564d47fdfd5ad3
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-11-09 13:55:21 -0300

    cambiar parse por apply y headOption en anychar

--- scala/src/main/scala/domain/Parsers.scala
+++ scala/src/main/scala/domain/Parsers.scala
@@ -6,14 +6,14 @@ import scala.util.{Failure, Success, Try}
 class ParserError(message: String) extends Exception(message)
 
 sealed trait Parser[T] {
-  def parse(s: String): Try[(T, String)]
+  def apply(s: String): Try[(T, String)]
 
   @targetName("or")
   def <|>[U](right: Parser[U]): Parser[T | U] = {
     val left = this
     new Parser[T | U] {
-      def parse(s: String): Try[(T | U, String)] = {
-        left.parse(s).recoverWith(_ => right.parse(s))
+      def apply(s: String): Try[(T | U, String)] = {
+        left(s).recoverWith(_ => right(s))
       }
     }
   }
@@ -22,10 +22,10 @@ sealed trait Parser[T] {
   def <>[U](right: Parser[U]): Parser[(T, U)] = {
     val left = this
     new Parser[(T, U)] {
-      def parse(s: String): Try[((T, U), String)] = {
+      def apply(s: String): Try[((T, U), String)] = {
         for {
-          (v1, r1) <- left.parse(s)
-          (v2, r2) <- right.parse(r1)
+          (v1, r1) <- left(s)
+          (v2, r2) <- right(r1)
         } yield ((v1, v2), r2)
       }
     }
@@ -35,10 +35,10 @@ sealed trait Parser[T] {
   def ~>[U](right: Parser[U]): Parser[U] = {
     val left = this
     new Parser[U] {
-      def parse(s: String): Try[(U, String)] = {
+      def apply(s: String): Try[(U, String)] = {
         for {
-          (_, r1) <- left.parse(s)
-          (v2, r2) <- right.parse(r1)
+          (_, r1) <- left(s)
+          (v2, r2) <- right(r1)
         } yield (v2, r2)
       }
     }
@@ -48,10 +48,10 @@ sealed trait Parser[T] {
   def <~[U](right: Parser[U]): Parser[T] = {
     val left = this
     new Parser[T] {
-      def parse(s: String): Try[(T, String)] = {
+      def apply(s: String): Try[(T, String)] = {
         for {
-          (v1, r1) <- left.parse(s)
-          (v2, r2) <- right.parse(r1)
+          (v1, r1) <- left(s)
+          (_, r2) <- right(r1)
         } yield (v1, r2)
       }
     }
@@ -60,11 +60,11 @@ sealed trait Parser[T] {
   def sepBy(sep: Parser[_]): Parser[List[T]] = {
     val contenido = this
     new Parser[List[T]] {
-      def parse(s: String): Try[(List[T], String)] = {
-        contenido.parse(s) // Llamada inicial
+      def apply(s: String): Try[(List[T], String)] = {
+        contenido(s) // Llamada inicial
           .flatMap((v1, r1) =>
-            sep.parse(r1).map(_._2) // Intentar parsear el separador y ignorar el resultado
-              .flatMap(this.parse) // Llamada recursiva, solo si el separador es exitoso
+            sep(r1).map(_._2) // Intentar parsear el separador y ignorar el resultado
+              .flatMap(this.apply) // Llamada recursiva, solo si el separador es exitoso
               .fold(
                 _ => Success(List(v1), r1), // Si es un fallo, solo devolver el valor parseado
                 (vs, r2) => Success(v1 :: vs, r2) // Si no es un fallo concatenar los resultados
@@ -76,14 +76,13 @@ sealed trait Parser[T] {
 }
 
 case object anyChar extends Parser[Char] {
-  def parse(s: String): Try[(Char, String)] = s match {
-    case "" => Failure(new ParserError("No hay más caracteres"))
-    case s => Success((s.head, s.tail))
-  }
+  def apply(s: String): Try[(Char, String)] = s.headOption.fold
+    (Failure(new ParserError("No hay más caracteres")))
+    (h => Success((h, s.tail)))
 }
 
 case class char(c: Char) extends Parser[Char] {
-  def parse(s: String): Try[(Char, String)] = s match {
+  def apply(s: String): Try[(Char, String)] = s match {
     case "" => Failure(new ParserError("No hay más caracteres"))
     case s if s.head == c => Success((c, s.tail))
     case other => Failure(new ParserError(s"Se esperaba el caracter $c pero se encontró ${other.head}"))
@@ -91,7 +90,7 @@ case class char(c: Char) extends Parser[Char] {
 }
 
 case object digit extends Parser[Char] {
-  def parse(s: String): Try[(Char, String)] = s match {
+  def apply(s: String): Try[(Char, String)] = s match {
     case "" => Failure(new ParserError("No hay más caracteres"))
     case s if s.head.isDigit => Success((s.head, s.tail))
     case other => Failure(new ParserError(s"Se esperaba un dígito pero se encontró ${other.head}"))
@@ -99,14 +98,14 @@ case object digit extends Parser[Char] {
 }
 
 case class string(expected: String) extends Parser[String] {
-  def parse(s: String): Try[(String, String)] = s match {
+  def apply(s: String): Try[(String, String)] = s match {
     case s if s.startsWith(expected) => Success((expected, s.drop(expected.length)))
     case other => Failure(new ParserError(s"Se esperaba la cadena $expected pero se encontró $other"))
   }
 }
 
 case object number extends Parser[Int] {
-  def parse(s: String): Try[(Int, String)] = s match {
+  def apply(s: String): Try[(Int, String)] = s match {
     case "" => Failure(new ParserError("No hay más caracteres"))
     case s if s.head.isDigit => Success((s.takeWhile(_.isDigit).toInt, s.dropWhile(_.isDigit)))
     case other => Failure(new ParserError(s"Se esperaba un dígito pero se encontró ${other.head}"))
@@ -114,10 +113,10 @@ case object number extends Parser[Int] {
 }
 
 case object integer extends Parser[Int] {
-  def parse(s: String): Try[(Int, String)] = s match {
+  def apply(s: String): Try[(Int, String)] = s match {
     case "" => Failure(new ParserError("No hay más caracteres"))
-    case s if s.head.isDigit => number.parse(s)
-    case s if s.head == '-' => number.parse(s.tail) match {
+    case s if s.head.isDigit => number(s)
+    case s if s.head == '-' => number(s.tail) match {
       case Success((n, rest)) => Success((-n, rest))
       case Failure(e) => Failure(e)
     }
@@ -126,11 +125,11 @@ case object integer extends Parser[Int] {
 }
 
 case object double extends Parser[Double] {
-  def parse(s: String): Try[(Double, String)] = s match {
+  def apply(s: String): Try[(Double, String)] = s match {
     case "" => Failure(new ParserError("No hay más caracteres"))
-    case s => integer.parse(s) match {
+    case s => integer(s) match {
       case Success((start, rest)) => if (rest.headOption.contains('.')) {
-        number.parse(rest.tail) match {
+        number(rest.tail) match {
           case Success((end, rest2)) => Success((s"$start.$end".toDouble, rest2))
           case Failure(_) => Failure(new ParserError(s"Se esperaba un número entero pero se encontró ${rest.head}"))
         }
--- scala/src/test/scala/ParsersTest.scala
+++ scala/src/test/scala/ParsersTest.scala
@@ -5,175 +5,175 @@ class ParsersTest extends AnyFreeSpec {
 
   "anyChar" - {
     "debería fallar si no hay más caracteres" in {
-      assert(anyChar.parse("").isFailure)
+      assert(anyChar("").isFailure)
     }
 
     "debería devolver el primer caracter y el resto de la cadena" in {
-      assert(anyChar.parse("abc").get == ('a', "bc"))
+      assert(anyChar("abc").get == ('a', "bc"))
     }
   }
 
   "char" - {
     "debería fallar si no hay más caracteres" in {
-      assert(char('a').parse("").isFailure)
+      assert(char('a')("").isFailure)
     }
 
     "debería fallar si el primer caracter no es el esperado" in {
-      assert(char('a').parse("b").isFailure)
+      assert(char('a')("b").isFailure)
     }
 
     "debería devolver el primer caracter y el resto de la cadena si el primer caracter es el esperado" in {
-      assert(char('a').parse("abc").get == ('a', "bc"))
+      assert(char('a')("abc").get == ('a', "bc"))
     }
   }
 
   "digit" - {
     "debería fallar si no hay más caracteres" in {
-      assert(digit.parse("").isFailure)
+      assert(digit("").isFailure)
     }
 
     "debería fallar si el primer caracter no es un dígito" in {
-      assert(digit.parse("a").isFailure)
+      assert(digit("a").isFailure)
     }
 
     "debería devolver el primer caracter y el resto de la cadena si el primer caracter es un dígito" in {
-      assert(digit.parse("1abc").get == ('1', "abc"))
+      assert(digit("1abc").get == ('1', "abc"))
     }
   }
 
   "string" - {
     "debería fallar si la cadena esperada no está al principio" in {
-      assert(string("abc").parse("bcasdfs").isFailure)
+      assert(string("abc")("bcasdfs").isFailure)
     }
 
     "debería devolver la cadena esperada y el resto de la cadena si la cadena esperada está al principio" in {
-      assert(string("abc").parse("abcde").get == ("abc", "de"))
+      assert(string("abc")("abcde").get == ("abc", "de"))
     }
   }
 
   "integer" - {
     "debería fallar si no hay más caracteres" in {
-      assert(integer.parse("").isFailure)
+      assert(integer("").isFailure)
     }
 
     "debería devolver el número entero y el resto de la cadena si el primer caracter es un dígito" in {
-      assert(integer.parse("123abc").get == (123, "abc"))
+      assert(integer("123abc").get == (123, "abc"))
     }
 
     "debería devolver el número entero y el resto de la cadena si el primer caracter es un dígito y es negativo" in {
-      assert(integer.parse("-123abc").get == (-123, "abc"))
+      assert(integer("-123abc").get == (-123, "abc"))
     }
 
     "deberia solo devolver el primer numero entero" in {
-      assert(integer.parse("123-456").get == (123, "-456"))
+      assert(integer("123-456").get == (123, "-456"))
     }
 
     "deberia no parsear un guion solo" in {
-      assert(integer.parse("-sabasdf").isFailure)
+      assert(integer("-sabasdf").isFailure)
     }
   }
 
   "double" - {
     "debería fallar si no hay más caracteres" in {
-      assert(double.parse("").isFailure)
+      assert(double("").isFailure)
     }
 
     "debería devolver el número entero y el resto de la cadena si el primer caracter es un dígito" in {
-      assert(double.parse("123abc").get == (123, "abc"))
+      assert(double("123abc").get == (123, "abc"))
     }
 
     "debería devolver el número entero y el resto de la cadena si el primer caracter es un dígito y es negativo" in {
-      assert(double.parse("-123abc").get == (-123, "abc"))
+      assert(double("-123abc").get == (-123, "abc"))
     }
 
     "debería devolver el número decimal y el resto de la cadena si el primer caracter es un dígito y tiene parte decimal" in {
-      assert(double.parse("123.456abc").get == (123.456, "abc"))
+      assert(double("123.456abc").get == (123.456, "abc"))
     }
 
     "debería devolver el número decimal y el resto de la cadena si el primer caracter es un dígito y tiene parte decimal y es negativo" in {
-      assert(double.parse("-123.456abc").get == (-123.456, "abc"))
+      assert(double("-123.456abc").get == (-123.456, "abc"))
     }
 
     "deberia fallar si no hay una sucesion de digitos despues del punto" in {
-      assert(double.parse("123.abc").isFailure)
+      assert(double("123.abc").isFailure)
     }
 
     "debería fallar si el primer caracter no es un dígito" in {
-      assert(double.parse("abc").isFailure)
+      assert(double("abc").isFailure)
     }
   }
 
   "or (<|>)" - {
     "deberia devolver el primer parser si tiene exito" in {
-      assert((char('a') <|> integer).parse("abc").get == ('a', "bc"))
+      assert((char('a') <|> integer)("abc").get == ('a', "bc"))
     }
 
     "deberia devolver el segundo parser si tiene exito" in {
-      assert((char('a') <|> integer).parse("123abc").get == (123, "abc"))
+      assert((char('a') <|> integer)("123abc").get == (123, "abc"))
     }
 
     "deberia fallar si ambos parsers fallan" in {
-      assert((char('a') <|> integer).parse("bc").isFailure)
+      assert((char('a') <|> integer)("bc").isFailure)
     }
 
     "deberia darle prioridad al primer parser" in {
-      assert((integer <|> char('1')).parse("1bc").get == (1, "bc"))
+      assert((integer <|> char('1'))("1bc").get == (1, "bc"))
     }
   }
 
   "concat (<>)" - {
     "deberia concatenar dos parsers" in {
-      assert((char('a') <> char('b')).parse("abc").get == (('a', 'b'), "c"))
+      assert((char('a') <> char('b'))("abc").get == (('a', 'b'), "c"))
     }
 
     "deberia fallar si el primer parser falla" in {
-      assert((char('a') <> char('b')).parse("bc").isFailure)
+      assert((char('a') <> char('b'))("bc").isFailure)
     }
 
     "deberia fallar si el segundo parser falla" in {
-      assert((char('a') <> char('b')).parse("ac").isFailure)
+      assert((char('a') <> char('b'))("ac").isFailure)
     }
   }
 
   "rightmost (~>)" - {
     "deberia devolver el resultado del segundo" in {
-      assert((char('a') ~> char('b')).parse("abc").get == ('b', "c"))
+      assert((char('a') ~> char('b'))("abc").get == ('b', "c"))
     }
 
     "deberia fallar si el primer parser falla" in {
-      assert((char('a') ~> char('b')).parse("bc").isFailure)
+      assert((char('a') ~> char('b'))("bc").isFailure)
     }
 
     "deberia fallar si el segundo parser falla" in {
-      assert((char('a') ~> char('b')).parse("ac").isFailure)
+      assert((char('a') ~> char('b'))("ac").isFailure)
     }
   }
 
   "leftmost (<~)" - {
     "deberia devolver el resultado del segundo" in {
-      assert((char('a') <~ char('b')).parse("abc").get == ('a', "c"))
+      assert((char('a') <~ char('b'))("abc").get == ('a', "c"))
     }
 
     "deberia fallar si el primer parser falla" in {
-      assert((char('a') <~ char('b')).parse("bc").isFailure)
+      assert((char('a') <~ char('b'))("bc").isFailure)
     }
 
     "deberia fallar si el segundo parser falla" in {
-      assert((char('a') <~ char('b')).parse("ac").isFailure)
+      assert((char('a') <~ char('b'))("ac").isFailure)
     }
   }
 
   "sepBy" - {
     "deberia devolver una lista de elementos" in {
-      assert((integer sepBy char(',')).parse("123,456,789").get == (List(123, 456, 789), ""))
+      assert((integer sepBy char(','))("123,456,789").get == (List(123, 456, 789), ""))
     }
 
     "deberia fallar si el primer parser falla" in {
-      assert((char('a') sepBy char(',')).parse("b,a,a").isFailure)
+      assert((char('a') sepBy char(','))("b,a,a").isFailure)
     }
 
     "deberia dar el primer valor si el segundo parser falla" in {
-      assert((char('a') sepBy char(',')).parse("a.a,b").get == (List('a'), ".a,b"))
+      assert((char('a') sepBy char(','))("a.a,b").get == (List('a'), ".a,b"))
     }
   }
 }
